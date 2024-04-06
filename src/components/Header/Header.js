e9474ebbecc68dd9d94a0aa69b2cc713365c6a1c
Heilong <llybolayo@gmail.com>
2024-04-06 19:52:57 +0200

    header and morning files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,64 @@
-import React from 'react'
-import './header.css'
+import React, { useState, useEffect } from 'react';
+import './header.css';
 
-function Header() {
+const Header = () => {
+  const [name, setName] = useState(() => localStorage.getItem('name') || '');
+  const [typedHello, setTypedHello] = useState('');
+  const [typedName, setTypedName] = useState('');
+  const [startTyping, setStartTyping] = useState(false);
+  const helloText = "Hello, ";
+  const delay = 200; 
+
+  useEffect(() => {
+    let timer;
+    if (startTyping) {
+      // Typing "Hello"
+      timer = setTimeout(() => {
+        const nextChar = helloText[typedHello.length];
+        if (nextChar) {
+          setTypedHello(typedHello + nextChar);
+        } else {
+          timer = setTimeout(() => {
+            const nextChar = name[typedName.length];
+            if (nextChar) {
+              setTypedName(typedName + nextChar);
+            }
+          }, delay);
+        }
+      }, delay);
+    }
+    return () => clearTimeout(timer);
+  }, [typedHello, typedName, startTyping, name]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setStartTyping(true);
+    }
+  };
+
+  // Save name to local storage when it changes
+  useEffect(() => {
+    localStorage.setItem('name', name);
+  }, [name]);
+
   return (
     <div className='container'>
-        <h1 className='header'>Hello, --friend-- </h1>
-        <hr className='headerhr' />
+      <h1 className='header'>{typedHello} {startTyping && <span className='name'>{typedName}</span>}</h1>
+      <h4>- Fake it till you make it -</h4>
+      <hr className='headerhr' />
+      {!startTyping && (
+        <input
+          className='input1'
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onKeyDown={handleKeyDown}
+          autoFocus
+        />
+      )}
     </div>
   );
 };
 
-export default TypingHeader;
+export default Header;
